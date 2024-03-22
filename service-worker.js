8ce887900448627bea93311e015ed3b1d7197010
DaeHee Han <63997481+hdh4952@users.noreply.github.com>
2024-03-23 01:31:50 +0900

    feat: 학사일정 데이터 가져오기

--- service-worker.js
+++ service-worker.js
@@ -8,8 +8,52 @@ function openModal() {
   });
 }
 
-chrome.runtime.onInstalled.addListener(({ reason }) => {
+async function getSchedules() {
+  const { tabs } = await chrome.windows.create({
+    url: 'https://onestop.pusan.ac.kr',
+    state: 'minimized',
+  });
+
+  const result = await chrome.scripting.executeScript({
+    target: { tabId: tabs[0].id },
+    func: () => {
+      const liTags = Array.from(document.querySelectorAll('.schedule-list > li'));
+      return liTags.map((li) => {
+        const content = li.querySelector('.subject').textContent;
+        const duration = li.querySelector('.list-date').textContent;
+        return { content, duration };
+      });
+    },
+  });
+
+  chrome.windows.remove(tabs[0].windowId);
+
+  return result[0].result;
+}
+
+/** 모달창에 표시할 데이터 */
+class ModalData {
+  /**
+   *
+   * @param {string} content
+   * @param {number} remainingDays
+   */
+  constructor(content, remainingDays) {
+    this.content = content;
+    this.remainingDays = remainingDays;
+  }
+}
+
+chrome.runtime.onInstalled.addListener(async ({ reason }) => {
   if (reason === 'install' || reason === 'update') {
-    openModal();
+    // openModal();
+    const schedules = await getSchedules();
+    console.log('schedules:', schedules);
+  }
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.requestModalData) {
+    sendResponse({ modalData: new ModalData('종강', 60) });
   }
 });
